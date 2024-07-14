1b8035c43d3526eca2c6dc61831d9f671326ceb0
Apotheous <brucoglu@outlook.com>
2024-07-15 02:49:36 +0300

    seller edits

--- Assets/Scripts/MrSeller/MrSellerManager.cs
+++ Assets/Scripts/MrSeller/MrSellerManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 
 public class MrSellerManager : MonoBehaviour
 {
+    public TextMeshProUGUI mrSellerText;
     public List<GameObject> ListCarSale = new List<GameObject>();
     public List<GameObject> SoldCarList= new List<GameObject>();
 
@@ -27,6 +28,10 @@ public class MrSellerManager : MonoBehaviour
 
     void Start()
     {
+        foreach (Transform item in content.transform)
+        {
+            carBtn.Add(item.transform.gameObject);
+        }
         GameObject[] allObjects = GameObject.FindObjectsOfType<GameObject>();
         foreach (GameObject obj in allObjects)
         {
@@ -35,7 +40,11 @@ public class MrSellerManager : MonoBehaviour
                 ListCarSale.Add(obj);
                 if (ListCarSale.Count > carBtn.Count) 
                 { 
-                    Instantiate(carBtnPref, content.transform); 
+                    Instantiate(carBtnPref, content.transform);
+                    carBtnPref.name = obj.name;
+                    carBtnPref.SetActive(true);
+                    carBttnText = carBtnPref.transform.GetChild(0).GetComponent<Text>();
+                    carBttnText.text = obj.name;
                 }
             }
         }
@@ -43,11 +52,8 @@ public class MrSellerManager : MonoBehaviour
        
         for (int i = 0; i < ListCarSale.Count; i++)
         {
-            carBtn[i].name= ListCarSale[i].name;    
+            carBtn[i].name= ListCarSale[i].name.ToString();    
             carBtn[i].SetActive(true);
-            Debug.Log("carBtnList"+ carBtn[i].name);
-            Debug.Log("carBtnList"+ carBtn[i].transform.GetChild(0).name);
-            Debug.Log("carBtnList"+ ListCarSale[i].name);
             carBttnText = carBtn[i].transform.GetChild(0).GetComponent<Text>();
             carBttnText.text= ListCarSale[i].name;
         }
@@ -56,7 +62,7 @@ public class MrSellerManager : MonoBehaviour
 
     public void SelectCar()
     {
-        textElements[0].text = BttnSelectCar.GetComponent<Car>().carObject.name;
+        textElements[0].text = BttnSelectCar.GetComponent<Car>().carObject.name.ToString();
         textElements[1].text = BttnSelectCar.GetComponent<Car>().carObject.damagedParts.ToString();
         textElements[2].text = BttnSelectCar.GetComponent<Car>().carObject.paintedParts.ToString();
         textElements[3].text = BttnSelectCar.GetComponent<Car>().carObject.maxSpeed.ToString();
@@ -64,7 +70,13 @@ public class MrSellerManager : MonoBehaviour
         textElements[5].text = BttnSelectCar.GetComponent<Car>().carObject.Suspensions.ToString();
         textElements[6].text = BttnSelectCar.GetComponent<Car>().carObject.wheelCamberValues.ToString();
         textElements[7].text = BttnSelectCar.GetComponent<Car>().carObject.price.ToString();
+        //MrSellerTextMetod("Seçtiðin arabanýn özelliklerini yanýmýzda ki panelde görebilirsin.");
+        mrSellerText.text="Seçtiðin arabanýn özelliklerini yanýmýzda ki panelde görebilirsin.";
     }
 
+    void MrSellerTextMetod(string text)
+    {
+        mrSellerText.text =  text;
+    }
 
 }
