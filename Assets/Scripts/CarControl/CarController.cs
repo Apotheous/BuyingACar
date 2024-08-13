c43f2f2a7e4863cb2b82fa3e6dfaa936ab55f966
Apotheous <brucoglu@outlook.com>
2024-08-13 13:22:55 +0300

    fonction regions

--- Assets/Scripts/CarControl/CarController.cs
+++ Assets/Scripts/CarControl/CarController.cs
@@ -75,27 +75,13 @@ public class CarController : MonoBehaviour,ISelectionCar
     public float stabilizerForce;
     public float downforceValue;
 
-
-    /// <summary>
-    /// ForInformation
-    /// </summary>
-    public float motorTorque = 2000;
-    public float brakeTorque = 2000;
-    public float maxSpeedII = 20;
-    public float steeringRange = 30;
-    public float steeringRangeAtMaxSpeed = 10;
-    public float centreOfGravityOffset = -1f;
-
-    public float forwardSpeed;
-    public float speedFactor;
-    public float currentMotorTorque;
-    public float currentSteerRange;
-    public bool isAccelerating;
     //CrashControl
     public float crashFactor;
 
 
     public float brakeInput;
+
+
     void Awake()
     {
         camVariables.drvCamGameObj = GameObject.Find("Drive Cam");
@@ -133,74 +119,22 @@ public class CarController : MonoBehaviour,ISelectionCar
 
     }
 
-    private void ForInformation()
+    private void FixedUpdate()
     {
-        // Calculate current speed in relation to the forward direction of the car
-        // (this returns a negative number when traveling backwards)
-        // Arabanýn ileri yönüne göre mevcut hýzý hesapla
-        // (geri doðru seyahat ederken negatif bir sayý döndürür)
-         forwardSpeed = Vector3.Dot(transform.forward, rb.velocity);
-
-
-        // Calculate how close the car is to top speed
-        // as a number from zero to one
-        // Arabanýn azami hýza ne kadar yakýn olduðunu hesapla
-        // sýfýrdan bire kadar bir sayý olarak
-         speedFactor = Mathf.InverseLerp(0, maxSpeed, forwardSpeed);
-
-        // Use that to calculate how much torque is available 
-        // (zero torque at top speed)
-        // Ne kadar torkun mevcut olduðunu hesaplamak için bunu kullan
-        // (azami hýzda sýfýr tork)
-         currentMotorTorque = Mathf.Lerp(motorTorque, 0, speedFactor);
-
-        // …and to calculate how much to steer 
-        // (the car steers more gently at top speed)
-        // …ve ne kadar direksiyon çevrileceðini hesaplamak için
-        // (araba en yüksek hýzda daha yumuþak bir þekilde direksiyon çevriliyor)
-         currentSteerRange = Mathf.Lerp(steeringRange, steeringRangeAtMaxSpeed, speedFactor);
-
-        // Check whether the user input is in the same direction 
-        // as the car's velocity
-        // Kullanýcý giriþinin aracýn hýzýyla ayný yönde olup olmadýðýný kontrol edin
-         isAccelerating = Mathf.Sign(verticalInput) == Mathf.Sign(forwardSpeed);
-
+        GetInput();
+        CheckDirection();
+        VerticalInputZero();
+        DragForce();
+        HandleMotor();
+        HandleSteering();
+        UpdateWheels();
+        MaxSpeed();
+        ApplyDownforce();
+        ApplyHandBreaking();
     }
-    public void GetInTheCar()
-    {
-        if (carObj.IsActive == true && mySeller.SoldCarList.Contains(gameObject))
-        {
+    
 
-            camVariables.driveCam.SetParent(carObj.transform);
-            camVariables.followPoint.SetParent(carObj.transform);
-            camVariables.driveCam.gameObject.SetActive(true);
-            characterCs.gameObject.SetActive(false);
-            characterCs.transform.SetParent(carObj.transform);
-
-            Vector3 carPos = new Vector3(carObj.transform.position.x, carObj.transform.position.y+1f, carObj.transform.position.z);
-            carObj.GetComponent<Rigidbody>().isKinematic = false;
-            camVariables.followPoint.position = carPos;
-            camVariables.followPoint.rotation = carObj.transform.rotation;
-
-            carObj.transform.gameObject.GetComponent<CarController>().enabled = true;
-            camVariables.interactirSource.GetComponent<Interactor>().inCar = true;
-            camVariables.interactirSource.GetComponent<Interactor>().theCarImin = carObj.transform;
-            carCanvas.SetActive(false);
-        }
-    }
-
-    public void GetOutOfTheCar()
-    {
-        camVariables.interactirSource.transform.position = camVariables.driveCam.position;
-        camVariables.driveCam.SetParent(null);
-        camVariables.driveCam.gameObject.SetActive(false);
-        characterCs.transform.SetParent(null);
-        characterCs.gameObject.SetActive(true);
-        carObj.GetComponent<Rigidbody>().isKinematic = true;
-        carObj.transform.gameObject.GetComponent<CarController>().enabled = false;
-        camVariables.interactirSource.GetComponent<Interactor>().inCar = false;
-        carCanvas.SetActive(true);
-    }
+    #region Propeties Assignment
     void SupensionCase()
     {
         switch (carObj.carObject.Suspensions)
@@ -226,63 +160,19 @@ public class CarController : MonoBehaviour,ISelectionCar
         SetSuspensionSpringTargetPosition(rearLeftWheelCollider, rearRightWheelCollider, rearWheels);
     }
 
-    private void FixedUpdate()
+    void SetSuspensionSpringTargetPosition(WheelCollider leftWheelCollider, WheelCollider rightWheelCollider, float targetPosition)
     {
-        GetInput();
-        ForInformation();
-        CheckDirection();
-        VerticalInputZero();
-        DragForce();
-        HandleMotor();
-        HandleSteering();
-        UpdateWheels();
-        MaxSpeed();
-        ApplyDownforce();
+        JointSpring suspensionSpring = leftWheelCollider.suspensionSpring;
 
-        ApplyHandBreaking();
+        suspensionSpring.targetPosition = Mathf.Clamp01(targetPosition);
+
+        leftWheelCollider.suspensionSpring = suspensionSpring;
+        rightWheelCollider.suspensionSpring = suspensionSpring;
     }
 
-    private void GetInput()
-    {
-        // Steering Input
-        horizontalInput = Input.GetAxis("Horizontal");
+    #endregion
 
-        // Acceleration Input
-        verticalInput = Input.GetAxis("Vertical");
-
-
-        //fixed code to brake even after going on reverse by Andrew Alex 
-        float movingDirection = Vector3.Dot(transform.forward, rb.velocity);
-        if (movingDirection < -0.5f && verticalInput > 0)
-        {
-            brakeInput = Mathf.Abs(verticalInput);
-        }
-        else if (movingDirection > 0.5f && verticalInput < 0)
-        {
-            brakeInput = Mathf.Abs(verticalInput);
-        }
-        else
-        {
-            brakeInput = 0;
-        }
-    }
-
-    private void VerticalInputZero()
-    {
-        if (verticalInput == 0)
-        {
-            // Arabanýn hýzýný yavaþça azaltmak için Lerp kullanýyoruz
-            float decelerationRate = 0.2f; // Yavaþlama hýzý
-            rb.velocity = Vector3.Lerp(rb.velocity, Vector3.zero, decelerationRate * Time.deltaTime);
-        }
-        else
-        {
-            // Input olduðu sürece mevcut hýzý saklýyoruz
-            currentSpeed = rb.velocity.magnitude;
-        }
-    }
-
-
+    #region Autonomous Controls
     private void DragForce()
     {
         if (verticalInput == 0)
@@ -305,6 +195,59 @@ public class CarController : MonoBehaviour,ISelectionCar
             currentDrag = initialDrag;
             rb.drag = currentDrag;
             transitionProgress = 0f;
+        }
+    }
+
+    private void MaxSpeed()
+    {
+        speed = rb.velocity.magnitude;
+        if (rb.velocity.magnitude > maxSpeed)
+        {
+            rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
+        }
+    }
+    private void VerticalInputZero()
+    {
+        if (verticalInput == 0)
+        {
+            // Arabanýn hýzýný yavaþça azaltmak için Lerp kullanýyoruz
+            float decelerationRate = 0.2f; // Yavaþlama hýzý
+            rb.velocity = Vector3.Lerp(rb.velocity, Vector3.zero, decelerationRate * Time.deltaTime);
+        }
+        else
+        {
+            // Input olduðu sürece mevcut hýzý saklýyoruz
+            currentSpeed = rb.velocity.magnitude;
+        }
+    }
+    private void ApplyDownforce()
+    {
+        rb.AddForce(-transform.up * downforceValue);
+    }
+    #endregion
+
+    #region Inputs and Informations
+    private void GetInput()
+    {
+        // Steering Input
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // Acceleration Input
+        verticalInput = Input.GetAxis("Vertical");
+
+        //fixed code to brake even after going on reverse by Andrew Alex 
+        float movingDirection = Vector3.Dot(transform.forward, rb.velocity);
+        if (movingDirection < -0.5f && verticalInput > 0)
+        {
+            brakeInput = Mathf.Abs(verticalInput);
+        }
+        else if (movingDirection > 0.5f && verticalInput < 0)
+        {
+            brakeInput = Mathf.Abs(verticalInput);
+        }
+        else
+        {
+            brakeInput = 0;
         }
     }
     private void CheckDirection()
@@ -331,13 +274,61 @@ public class CarController : MonoBehaviour,ISelectionCar
             Debug.Log("Araba duruyor.");
         }
     }
+
+    #endregion
+
+    #region Procedures performed when getting in and out of a car
+
+    public void GetInTheCar()
+    {
+        if (carObj.IsActive == true && mySeller.SoldCarList.Contains(gameObject))
+        {
+
+            camVariables.driveCam.SetParent(carObj.transform);
+            camVariables.followPoint.SetParent(carObj.transform);
+            camVariables.driveCam.gameObject.SetActive(true);
+            characterCs.gameObject.SetActive(false);
+            characterCs.transform.SetParent(carObj.transform);
+
+            Vector3 carPos = new Vector3(carObj.transform.position.x, carObj.transform.position.y + 1f, carObj.transform.position.z);
+            carObj.GetComponent<Rigidbody>().isKinematic = false;
+            camVariables.followPoint.position = carPos;
+            camVariables.followPoint.rotation = carObj.transform.rotation;
+
+            carObj.transform.gameObject.GetComponent<CarController>().enabled = true;
+            camVariables.interactirSource.GetComponent<Interactor>().inCar = true;
+            camVariables.interactirSource.GetComponent<Interactor>().theCarImin = carObj.transform;
+            carCanvas.SetActive(false);
+        }
+    }
+
+    public void GetOutOfTheCar()
+    {
+        camVariables.interactirSource.transform.position = camVariables.driveCam.position;
+        camVariables.driveCam.SetParent(null);
+        camVariables.driveCam.gameObject.SetActive(false);
+        characterCs.transform.SetParent(null);
+        characterCs.gameObject.SetActive(true);
+        carObj.GetComponent<Rigidbody>().isKinematic = true;
+        carObj.transform.gameObject.GetComponent<CarController>().enabled = false;
+        camVariables.interactirSource.GetComponent<Interactor>().inCar = false;
+        carCanvas.SetActive(true);
+    }
+
+    #endregion
+
+    #region Functions that enable the car to move
     private void HandleMotor()
     {
         rearLeftWheelCollider.motorTorque = verticalInput * motorForce;
         rearRightWheelCollider.motorTorque = verticalInput * motorForce;
-
     }
-
+    private void HandleSteering()
+    {
+        currentSteerAngle = maxSteerAngle * horizontalInput;
+        frontLeftWheelCollider.steerAngle = currentSteerAngle;
+        frontRightWheelCollider.steerAngle = currentSteerAngle;
+    }
 
     private void ApplyHandBreaking()
     {
@@ -346,31 +337,13 @@ public class CarController : MonoBehaviour,ISelectionCar
 
         rearLeftWheelCollider.brakeTorque = brakeInput * handBreakForce * 0.3f;
         rearRightWheelCollider.brakeTorque = brakeInput * handBreakForce * 0.3f;
-        //frontRightWheelCollider.brakeTorque = currentHandBreakForce;
-        //frontLeftWheelCollider.brakeTorque = currentHandBreakForce;
-        //rearLeftWheelCollider.brakeTorque = currentHandBreakForce;
-        //rearRightWheelCollider.brakeTorque = currentHandBreakForce;
     }
+    #endregion
 
-    private void HandleSteering()
+    #region Turn the wheels (in accordance with the camber value)
+    private void UpdateSingleWheel(WheelCollider wheelCollider, Transform wheelTransform, float cmbrAbngle)
     {
-        currentSteerAngle = maxSteerAngle * horizontalInput;
-        frontLeftWheelCollider.steerAngle = currentSteerAngle;
-        frontRightWheelCollider.steerAngle = currentSteerAngle;
-    }
-
-    private void UpdateWheels()
-    {
-        UpdateSingleWheel(frontLeftWheelCollider, frontLeftWheelTransform, -camberAngle);
-        UpdateSingleWheel(frontRightWheelCollider, frontRightWheelTransform, camberAngle);
-        UpdateSingleWheel(rearLeftWheelCollider, rearLeftWheelTransform, -camberAngle);
-        UpdateSingleWheel(rearRightWheelCollider, rearRightWheelTransform, camberAngle);
-
-    }
-
-    private void UpdateSingleWheel(WheelCollider wheelCollider, Transform wheelTransform,float cmbrAbngle)
-    {
-        if (cmbrAbngle>=0)
+        if (cmbrAbngle >= 0)
         {
             camberAngle = camberAngle;
         }
@@ -383,28 +356,13 @@ public class CarController : MonoBehaviour,ISelectionCar
         wheelTransform.transform.localRotation = Quaternion.Euler(0, wheelCollider.steerAngle, camberAngle);
         wheelTransform.transform.GetChild(0).transform.Rotate(wheelCollider.rpm * 0.01f * Time.deltaTime, 0, 0, Space.Self);
         wheelTransform.transform.position = wheelPosition;
-
     }
-    void SetSuspensionSpringTargetPosition(WheelCollider leftWheelCollider, WheelCollider rightWheelCollider, float targetPosition)
+    private void UpdateWheels()
     {
-        JointSpring suspensionSpring = leftWheelCollider.suspensionSpring;
-
-        suspensionSpring.targetPosition = Mathf.Clamp01(targetPosition);
-
-        leftWheelCollider.suspensionSpring = suspensionSpring;
-        rightWheelCollider.suspensionSpring = suspensionSpring;
+        UpdateSingleWheel(frontLeftWheelCollider, frontLeftWheelTransform, -camberAngle);
+        UpdateSingleWheel(frontRightWheelCollider, frontRightWheelTransform, camberAngle);
+        UpdateSingleWheel(rearLeftWheelCollider, rearLeftWheelTransform, -camberAngle);
+        UpdateSingleWheel(rearRightWheelCollider, rearRightWheelTransform, camberAngle);
     }
-    private void MaxSpeed()
-    {
-        speed = rb.velocity.magnitude;
-        if (rb.velocity.magnitude > maxSpeed)
-        {
-            rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
-        }
-    }
-
-    private void ApplyDownforce()
-    {
-        rb.AddForce(-transform.up * downforceValue);
-    }
+    #endregion
 }
