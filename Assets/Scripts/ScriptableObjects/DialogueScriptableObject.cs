38bca86bb2d961790677ce9594f9665a90a462fe
Apotheous <brucoglu@outlook.com>
2024-08-16 16:36:32 +0300

    texMeshPro pack update

--- Assets/Scripts/ScriptableObjects/DialogueScriptableObject.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "DialogueScriptableObject", menuName = "ScriptableObjects/DialogueScriptableObject")]
-public class DialogueScriptableObject : ScriptableObject
-{
-    public string[] textStart;
-}
